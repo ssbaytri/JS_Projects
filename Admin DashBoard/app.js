47bb1a0b04640f4ee745fa0ba903efde59985fda
SubGameR69e <azjoker333@gmail.com>
2023-11-24 21:42:12 +0100

    Main section is Done

--- Admin DashBoard/app.js
+++ Admin DashBoard/app.js
@@ -10,3 +10,13 @@ function activeLink() {
 
 
 list.forEach(item => item.addEventListener("mouseover", activeLink));
+
+// menu toggle
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.onclick = function() {
+	navigation.classList.toggle("active");
+	main.classList.toggle("active");
+}
