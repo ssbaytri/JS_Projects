0018b9d79a3a6dbd8fc7601fe340b6b3b3302e90
SubGameR69e <azjoker333@gmail.com>
2024-02-17 23:56:53 +0100

    Bottom nav & Header Slider

--- Amazon Clone/main.js
+++ Amazon Clone/main.js
@@ -1,1 +1,32 @@
-// strat of the project
+const imgs = document.querySelectorAll(".header-slider ul img");
+const prevBtn = document.querySelector(".control-prev");
+const nextBtn = document.querySelector(".control-next");
+
+let n = 0;
+
+function changeSlide() {
+    for(let i = 0; i < imgs.length; i++){
+        imgs[i].style.display = "none";
+    }
+    imgs[n].style.display = "block";
+}
+
+changeSlide();
+
+prevBtn.addEventListener("click", (e)=>{
+    if (n > 0) {
+        n--;
+    }else{
+        n = imgs.length - 1;
+    }
+    changeSlide();
+})
+
+nextBtn.addEventListener("click", (e)=>{
+    if (n < imgs.length - 1) {
+        n++;
+    }else{
+        n = 0;
+    }
+    changeSlide();
+})
