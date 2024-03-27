b610d9b0936c359fd73f1345d894a17be0cd6b36
SubGameR69e <azjoker333@gmail.com>
2024-03-27 22:08:11 +0000

    End of Project.

--- Temperature Converter/app.js
+++ Temperature Converter/app.js
@@ -1,1 +1,18 @@
 // start of project
+const celsius = document.getElementById("celsius");
+const fahrenheit = document.getElementById("fahrenheit");
+
+// focus on input after load
+window.addEventListener("load", () => celsius.focus());
+
+// convert celsius to fahrenheit
+celsius.addEventListener("input", () => {
+    fahrenheit.value = ((celsius.value * 9) / 5 + 32).toFixed(2);
+    if (!celsius.value) fahrenheit.value = "";
+})
+
+// convert fahrenheit to celsius
+fahrenheit.addEventListener("input", () => {
+    celsius.value = ((fahrenheit.value - 32) * 5 / 9).toFixed(2);
+    if (!fahrenheit.value) celsius.value = "";
+})
