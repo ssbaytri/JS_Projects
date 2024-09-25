9ef0c6d69bb92ba3ca6adeca0893b05d22fcc495
SubGameR69e <azjoker333@gmail.com>
2024-09-25 20:03:00 +0100

    dark-btn

--- Facebook Clone/script.js
+++ Facebook Clone/script.js
@@ -1,6 +1,10 @@
 var settingsMenu = document.querySelector(".settings-menu")
-
+var darkBtn = document.getElementById("dark-btn");
 
 function settingsMenuToggle() {
 	settingsMenu.classList.toggle("settings-menu-height");
+}
+
+function darkTheme() {
+	darkBtn.classList.toggle("dark-btn-on");
 }
