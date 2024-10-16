11f8f84d0830ed52b0ac8518aa96c4cb30bcecb8
SubGameR69e <azjoker333@gmail.com>
2024-10-16 22:03:24 +0100

    media screen

--- LinkedIn Clone/script.js
+++ LinkedIn Clone/script.js
@@ -1,6 +1,18 @@
 let profileMenu = document.getElementById("profile-menu");
+let sidebarActivity = document.querySelector(".sidebar-activity");
+let moreLink = document.getElementById("show-more-link");
 
 function toggleMenu() {
 	profileMenu.classList.toggle("open-menu");
+}
+
+function toggleActivity() {
+	sidebarActivity.classList.toggle("open");
+	if (sidebarActivity.classList.contains("open")) {
+		moreLink.innerHTML = "Show less <b>-</b>"
+	}
+	else {
+		moreLink.innerHTML = "Show more <b>+</b>"
+	}
 }
 
